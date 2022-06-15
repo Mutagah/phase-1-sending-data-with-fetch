b89dfd5f51561ce6657bd051b8391f93dfef40e5
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-06-15 23:04:34 +0300

    commit tree 388b57c75288de81e0f5154ef88bd47a59c9b366

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(name, email){
+
+    const configurationObject = {
+    method: "POST",
+    headers : {
+        "Content-Type": "application/json",
+        Accept : "application/json",
+    },
+    body : JSON.stringify({
+        name: "Steve" ,
+        email: "steve@steve.com",
+    }), 
+    
+    };
+return fetch("http://localhost:3000/users", configurationObject)
+        .then(resp => resp.json())
+        .then(data => document.body.innerHTML = data.id )
+            .catch(error => document.body.innerHTML = error.message)
+}
